dfc8a00e033599bfe022ba2ffca9ce1cb67bae0f
Fabrice Quenneville <fabquenneville@gmail.com>
2021-09-28 14:30:54 -0400

    Bug fix and added shuffle feature

--- spotifytools/library/spotools.py
+++ spotifytools/library/spotools.py
@@ -4,6 +4,7 @@
 '''
 
 import os
+import random
 from spotipy.exceptions import SpotifyException
 
 from .tools import attach_liked_song, attach_playlist_song, getdb, save_playlist, save_song
@@ -18,7 +19,7 @@ def get_user_liked_songs(spotify, limit = 50):
     print(f"Found {len(tracks)} liked songs.")
     return tracks
 
-def get_user_playlists(spotify, username):
+def get_user_playlists(spotify, username, tracks = False):
     results     = spotify.user_playlists(username)
     playlists   = results['items']
     while results["next"]:
@@ -31,10 +32,14 @@ def get_user_playlists(spotify, username):
         if playlist["owner"]["id"] != username:
             continue
         pl = spotify.playlist(playlist['id'])
-        pl["tracks"]["items"] = get_playlist_tracks(spotify, playlist['id'])
+        if tracks:
+            pl["tracks"]["items"] = get_playlist_tracks(spotify, playlist['id'])
+            nb_tracks += len(pl["tracks"]["items"])
         playlists.append(pl)
-        nb_tracks += len(pl["tracks"]["items"])
-    print(f"Found {len(playlists)} playlists containing {nb_tracks} tracks.")
+    if tracks:
+        print(f"Found {len(playlists)} playlists containing {nb_tracks} tracks.")
+    else:
+        print(f"Found {len(playlists)} playlists.")
     return playlists
 
 def get_playlist_tracks(spotify, id):
@@ -45,21 +50,25 @@ def get_playlist_tracks(spotify, id):
         tracks.extend(results['items'])
     return tracks
 
-def empty_list(spotify, listname, tracks = False):
+def empty_list(spotify, username, listname, tracks = False):
     # Getting tracks to remove
     playlistid = False
     if listname.lower() not in ["liked songs", "liked"]:
-        playlistid = get_listid(spotify, listname)
+        playlistid = get_user_playlist_id(spotify, username, listname)
 
+    tracklist = list()
     if not tracks:
         if listname.lower() in ["liked songs", "liked"]:
             tracklist = get_user_liked_songs(spotify)
         else:
             tracklist = get_playlist_tracks(spotify, playlistid)
-        tracks = list()
-        for track in tracklist:
+    else:
+        tracklist = tracks
+    tracks = list()
+    for track in tracklist:
+        if isinstance(track, dict):
             tracks.append(track["track"]["id"])
-    
+
     if len(tracks) < 1:
         return False
     # Removing tracks
@@ -74,25 +83,24 @@ def empty_list(spotify, listname, tracks = False):
             spotify.playlist_remove_all_occurrences_of_items(playlistid, tracks[start:start+jumps])
         start += jumps
 
-def get_listid(spotify, listname):
-    result = spotify.search(q="playlist:" + listname, type="playlist", limit=1)
-    if len(result["playlists"]["items"]) < 1:
-        return False
-    if result["playlists"]["items"][0]["name"].lower() != listname.lower():
-        return False
-    return result["playlists"]["items"][0]["id"]
-
-def add_to_list(spotify, listname, tracks):
+def add_to_list(spotify, username, listname, tracks):
     listid = False
     if listname.lower() not in ["liked songs", "liked"]:
-        result = spotify.search(q="playlist:" + listname, type="playlist", limit=1)
-        if len(result["playlists"]["items"]) < 1:
-            return False
-        listid = result["playlists"]["items"][0]["id"]
+        listid = get_user_playlist_id(spotify, username, listname)
     start = 0
     jumps = 100
     if listname.lower() in ["liked songs", "liked"]:
         jumps = 20
+
+    # Handle raw spotify results > extract id's
+    tracklist = tracks
+    tracks = list()
+    for track in tracklist:
+        if isinstance(track, dict):
+            tracks.append(track["track"]["id"])
+        else:
+            tracks.append(track)
+    
     while start < len(tracks):
         if listname.lower() in ["liked songs", "liked"]:
             spotify.current_user_saved_tracks_add(tracks[start:start+jumps])
@@ -112,7 +120,7 @@ def run_backup(spotify, arguments, config):
         save_song(sqliteb, (track['track']['id'], track['track']['name'], track['track']['artists'][0]['name']))
         attach_liked_song(sqliteb, (track['track']['id'], ))
     # Saving playlists
-    playlists = get_user_playlists(spotify, config["main"]["account"])
+    playlists = get_user_playlists(spotify, config["main"]["account"], True)
     for playlist in playlists:
         save_playlist(sqliteb, (playlist['id'], playlist['name']))
         for track in playlist["tracks"]["items"]:
@@ -122,10 +130,10 @@ def run_backup(spotify, arguments, config):
             attach_playlist_song(sqliteb, (playlist['id'], track['track']['id']))
     
 
-def auto_unlike(spotify, config):
+def auto_unlike(spotify, username):
     likedsongs          = get_user_liked_songs(spotify)
     likedsongsids       = list()
-    playlists           = get_user_playlists(spotify, config["main"]["account"])
+    playlists           = get_user_playlists(spotify, username, True)
     playlistsongsids    = list()
     unlikeids           = list()
 
@@ -147,3 +155,50 @@ def auto_unlike(spotify, config):
         start += jumps
     print(f"Unliked {len(unlikeids)} songs")
 
+def copy_playlists(spotify, username, innames, outnames, delete = False):
+    for source in innames:
+        tracks = False
+        if source.lower() in ["liked songs", "liked"]:
+            tracks = get_user_liked_songs(spotify)
+        else:
+            # result = spotify.search(q="playlist:" + source, type="playlist", limit=1)
+            # if len(result["playlists"]["items"]) < 1:
+            #     continue
+            # if result["playlists"]["items"][0]["name"].lower() != source.lower():
+            #     continue
+            # tracks = get_playlist_tracks(spotify, result["playlists"]["items"][0]["id"])
+            listid = False
+            listid = get_user_playlist_id(spotify, username, source)
+            if listid:
+                tracks = get_playlist_tracks(spotify, listid)
+        tracklist = list()
+        for track in tracks:
+            tracklist.append(track["track"]["id"])
+        if len(tracklist) < 1:
+            continue
+        print(f"Found {len(tracklist)} tracks.")
+        for listname in outnames:
+            add_to_list(spotify, username, listname, tracklist)
+        if delete:
+            empty_list(spotify, username, source)
+
+def get_user_playlist_id(spotify, username, listname):
+    playlists = get_user_playlists(spotify, username)
+    for playlist in playlists:
+        if listname.lower() == playlist['name'].lower():
+            return playlist['id']
+    return False
+
+def shuffle_list(spotify, username, listname):
+    listid = False
+    tracks = list()
+    if listname.lower() not in ["liked songs", "liked"]:
+        listid = get_user_playlist_id(spotify, username, listname)
+        if listid:
+            tracks = get_playlist_tracks(spotify, listid)
+    else:
+        tracks = get_user_liked_songs(spotify)
+    if tracks:
+        empty_list(spotify, username, listname, tracks)
+        random.shuffle(tracks)
+        add_to_list(spotify, username, listname, tracks)
--- spotifytools/spotifytools.py
+++ spotifytools/spotifytools.py
@@ -6,12 +6,12 @@ import spotipy
 # Normal import
 try:
     from redbot.library.tools import load_arguments, load_config, create_tables, getdb, save_playlist, save_song, attach_liked_song, attach_playlist_song, json_print
-    from redbot.library.spotools import get_user_liked_songs, get_playlist_tracks, empty_list, add_to_list, run_backup, get_user_playlists, auto_unlike
+    from redbot.library.spotools import get_user_liked_songs, empty_list, run_backup, get_user_playlists, auto_unlike, shuffle_list, copy_playlists, get_user_playlist_id, get_playlist_tracks
     from redbot.library.lastfmtools import lastfm_get
 # Allow local import for development purposes
 except ModuleNotFoundError:
     from library.tools import load_arguments, load_config, create_tables, getdb, save_playlist, save_song, attach_liked_song, attach_playlist_song, json_print
-    from library.spotools import get_user_liked_songs, get_playlist_tracks, empty_list, add_to_list, run_backup, get_user_playlists, auto_unlike
+    from library.spotools import get_user_liked_songs, empty_list, run_backup, get_user_playlists, auto_unlike, shuffle_list, copy_playlists, get_user_playlist_id, get_playlist_tracks
     from library.lastfmtools import lastfm_get
 
 def main():
@@ -25,7 +25,7 @@ def main():
     # Establish API connection
     token = spotipy.util.prompt_for_user_token(
         username        = account,
-        scope           = "user-library-read user-library-modify playlist-read-private playlist-modify-private",
+        scope           = "user-library-read user-library-modify playlist-read-private playlist-modify-private playlist-modify-public",
         client_id       = config["spotify"]["id"],
         client_secret   = config["spotify"]["secret"],
         redirect_uri    = config["spotify"]["redirect_uri"]
@@ -41,33 +41,18 @@ def main():
     if arguments["backup"]:
         run_backup(spotify, arguments, config)
     if arguments["action"] in ["copy", "transfer"]:
-        for source in arguments["in"]:
-            tracks = False
-            if source.lower() in ["liked songs", "liked"]:
-                tracks = get_user_liked_songs(spotify)
-            else:
-                result = spotify.search(q="playlist:" + source, type="playlist", limit=1)
-                if len(result["playlists"]["items"]) < 1:
-                    continue
-                if result["playlists"]["items"][0]["name"].lower() != source.lower():
-                    continue
-                tracks = get_playlist_tracks(spotify, result["playlists"]["items"][0]["id"])
-            tracklist = list()
-            for track in tracks:
-                tracklist.append(track["track"]["id"])
-            if len(tracklist) < 1:
-                continue
-            print(f"Found {len(tracklist)} tracks.")
-            for listname in arguments["out"]:
-                add_to_list(spotify, listname, tracklist)
-            if arguments["action"] == "transfer":
-                empty_list(spotify, source)
+        delete = False
+        if arguments["action"] == "transfer":
+            delete = True
+        copy_playlists(spotify, config["main"]["account"], arguments["in"], arguments["out"], delete = False)
     elif arguments["action"] == "autounlike":
-        auto_unlike(spotify, config)
-
+        auto_unlike(spotify, config["main"]["account"])
     elif arguments["action"] == "empty":
         for listname in arguments["playlists"]:
-            empty_list(spotify, listname)
+            empty_list(spotify, config["main"]["account"], listname)
+    elif arguments["action"] == "shuffle":
+        for listname in arguments["playlists"]:
+            shuffle_list(spotify, config["main"]["account"], listname)
     elif arguments["action"] == "test":
         songs = get_user_liked_songs(spotify)
         for song in songs:
@@ -93,6 +78,11 @@ def main():
         spotify = spotipy.Spotify(auth=token)
         # print(token)
         # playlists = get_user_playlists(spotify, config["main"]["account"])
+    elif arguments["action"] == "fix1":
+        listid1 = get_user_playlist_id(spotify, config["main"]["account"], "test123")
+        if listid1:
+            tracks = get_playlist_tracks(spotify, listid1)
+        empty_list(spotify, config["main"]["account"], "Hip-Hop - Rap", tracks)
 
 if __name__ == '__main__':
     main()
